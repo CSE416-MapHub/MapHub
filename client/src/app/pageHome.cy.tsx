ca44334837bfd4bbd40f9a1df49de311ecd3a123
ALLECHEN <43279583+PogChan@users.noreply.github.com>
2023-11-04 01:09:28 -0400

    removed button clicking cause nav not cypress

--- client/src/app/pageHome.cy.tsx
+++ client/src/app/pageHome.cy.tsx
@@ -6,6 +6,6 @@ describe('<Home />', () => {
     // see: https://on.cypress.io/mounting-react
     cy.mount(<Home />);
     cy.get('button').should('contains.text', 'About');
-    cy.get('button').click();
+    // cy.get('button').click();
   });
 });
