d93ab4355edd6cbefdd626e6f98e807ec4c10c46
Wesley Mui <68392398+wesleymui@users.noreply.github.com>
2023-11-05 12:26:31 -0500

    Modify jest config file

--- server/jest.config.ts
+++ server/jest.config.ts
@@ -1,5 +1,10 @@
 module.exports = {
+  transform: {
+    '^.+\\.ts$': 'ts-jest', // Use 'ts-jest' for TypeScript files
+    '^.+\\.js$': 'babel-jest', // Use 'babel-jest' for JavaScript files
+  },
   preset: 'ts-jest',
   testEnvironment: 'node',
   modulePathIgnorePatterns: ['<rootDir>/build/'],
 };
+
