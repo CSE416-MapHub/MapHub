4e151704d1dd177211683f077fbd4945a01512e4
William Gao <william.gao09@gmail.com>
2023-11-05 11:52:21 -0500

    Made root path the default

--- client/next.config.js
+++ client/next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
+console.log(process.env)
 const nextConfig = {
-    basePath: process.env.SERVER === "prod" ? "/" : "/dev"
+    basePath: process.env.SERVER.trim() === "dev" ? "/dev" : ""
 }
 
 module.exports = nextConfig
