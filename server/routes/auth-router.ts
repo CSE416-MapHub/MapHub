55b5dce83a4493ca03b5a69fc2a29ed81bc25aec
Wesley Mui <68392398+wesleymui@users.noreply.github.com>
2023-11-06 11:31:30 -0500

    update auth router

--- server/routes/auth-router.ts
+++ server/routes/auth-router.ts
@@ -1,9 +1,9 @@
 import express from 'express';
-import AuthController from '../controllers/auth-controller'
+import * as AuthController from '../controllers/auth-controller';
 
 const router = express.Router()
 
-// router.post('/register', AuthController.registerUser)
+router.post('/register', AuthController.registerUser)
 // router.post('/login', AuthController.loginUser)
 // router.get('/logout', AuthController.logoutUser)
 // router.get('/loggedIn', AuthController.getLoggedIn)
