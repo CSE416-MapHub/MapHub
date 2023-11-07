5dac0ba0e1e1cd71a1c3e37e54bc29a8f043a808
ALLECHEN <43279583+PogChan@users.noreply.github.com>
2023-11-06 19:50:34 -0500

    modifed user schema for unique, changed dup user

--- server/models/user-model.ts
+++ server/models/user-model.ts
@@ -1,17 +1,17 @@
 import mongoose from 'mongoose';
 
-const Schema = mongoose.Schema
+const Schema = mongoose.Schema;
 
 const userSchema = new Schema(
-    {
-        username: { type: String, required: true },
-        email: { type: String, required: true },
-        password: { type: String, required: true },
-        profilePic: { type: Buffer, required: true },
-        maps: [{type: Schema.Types.ObjectId, required: true, ref: 'Map'}]
-    },
-    { timestamps: true },
-)
+  {
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    profilePic: { type: Buffer, required: true },
+    maps: [{ type: Schema.Types.ObjectId, required: true, ref: 'Map' }],
+  },
+  { timestamps: true }
+);
 
 const userModel = mongoose.model('User', userSchema);
 
