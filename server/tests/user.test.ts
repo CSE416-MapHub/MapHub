b70c4f3be39a6b26f1a0c1ec2b87297cb4f11420
ALLECHEN <43279583+PogChan@users.noreply.github.com>
2023-11-07 12:08:45 -0500

    created dup user regi and get all users tests

--- server/tests/user.test.ts
+++ server/tests/user.test.ts
@@ -1,14 +1,28 @@
 import request from 'supertest';
 import app from '../index';
-import "@types/jest"
 
+//we test using the same email all the time so we want t osee if the duplcation works
 describe('User Registration API', () => {
   it('should register a new user', async () => {
-    const response = await request(app).post('/api/register').send({
+    const response = await request(app).post('/auth/register').send({
       username: 'testuser',
-      password: 'testpassword',
+      email: 'mapperhubbers@gmail.com',
+      password: 'test!P1assword',
+      passwordVerify:'test!P1assword'
+    });
+    expect(response.status).toBe(400);
+    expect(response.body).toEqual({
+      success: false,
+      errorMessage: 'Username already in use.',
+    });
+  });
+});
+
+describe('User Retrieval API', () => {
+  it('should retrieve all registered users', async () => {
+    const response = await request(app).get('/auth/users').send({
     });
 
-    expect(response.status).toBe(201);
+    expect(response.status).toBe(200);
   });
 });
