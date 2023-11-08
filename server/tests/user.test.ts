cda8144fb8dc927e22c8c3bbf948156c2cc47aee
ALLECHEN <43279583+PogChan@users.noreply.github.com>
2023-11-07 23:35:54 -0500

    mongdbmemserver, 2 backend tests,

--- server/tests/user.test.ts
+++ server/tests/user.test.ts
@@ -1,8 +1,22 @@
 import request from 'supertest';
 import app from '../index';
+import * as db from './db'
+import mongoose from 'mongoose';
+
+beforeAll(async () => {
+
+  await db.connect()
+});
+afterEach(async () => {
+  await db.clearDatabase()
+});
+afterAll(async () => {
+  await db.closeDatabase()
+});
 
 //we test using the same email all the time so we want t osee if the duplcation works
 describe('User Registration API', () => {
+
   it('should register a new user', async () => {
     const response = await request(app).post('/auth/register').send({
       username: 'testuser',
@@ -26,3 +40,4 @@ describe('User Retrieval API', () => {
     expect(response.status).toBe(200);
   });
 });
+
