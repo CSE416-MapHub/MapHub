54c641132ae830fb7d1d1d077c2dfa6f4139aa5a
Wesley Mui <68392398+wesleymui@users.noreply.github.com>
2023-11-12 21:45:15 -0500

    Add home page

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -2,24 +2,68 @@ import React from 'react';
 import Link from 'next/link';
 import Button from '../components/button';
 import Typography from '@mui/material/Typography';
+import { Box, Container, colors } from '@mui/material';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <Box
+        sx={{
+          height: '80px', // Set the height to 80px
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center', // Center the content horizontally
+        }}
+      >
+        {/* TODO: Add NavBar here */}
       <Typography variant="h1">
-        MAPHUB - The BEST Social Media Map Editor
+        MAPHUB
       </Typography>
-      <Link href="/about">
-        <Button variant="filled">
-            About
-          </Button>
-        </Link>
-  
-      <Link href="/account/create">
-        <Button variant="filled">
-          Join Now
-        </Button>
-      </Link>
+      </Box>
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          width: '1513px',
+          height: '808px',
+          borderRadius: 32,
+          margin: '50px 191px 86px 50px',
+          bgcolor: 'primary.container'
+        }}
+      >
+        <Container sx={{
+          marginLeft: '50px',
+          marginBottom: '30px'
+        }}>
+          <Typography variant="h1">
+            A Complete <br/>
+            Map Visuals Studio.
+          </Typography>
+          <Typography variant='body1' sx={{marginTop: '10px'}}>
+            Five Essential Templates.<br/>
+            A Plethora of Editing Tools.<br/>
+            Unique data-driven approach.
+          </Typography>
+        </Container>
+        <Container sx={{
+          alignItems: 'center',
+          justifyContent: 'center',
+          mx: 'auto',
+          marginLeft: '50px'
+        }}>
+          <Link href="/about">
+            <Button variant="outlined" sx={{ marginRight: '16px' }}>
+                Discover Maps
+              </Button>
+            </Link>
+          <Link href="/account/login">
+          <Button variant="filled">
+              Start Creating
+            </Button>
+          </Link>
+        </Container>
+      </Box>
     </main>
   );
 }
