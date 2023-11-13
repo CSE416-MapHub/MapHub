980ec0c1a046f278961bbf2c26569f04777d641a
Edward Ng <edwardlng@outlook.com>
2023-11-12 23:58:55 -0500

    Created a navigation bar and added logo assets.

--- client/cypress/components/navBar.cy.tsx
+++ client/cypress/components/navBar.cy.tsx
@@ -5,51 +5,41 @@ import NavBar from '../../src/app/components/navBar';
 describe('Navigation Bar', () => {
   it('renders a home link.', () => {
     cy.mount(<NavBar />);
-    cy.get('[data-test="home"]').as('home-link');
-    cy.get('@home-link').contains('svg');
+    cy.get('#home').as('home-link');
+    cy.get('@home-link').children('img').should('be.visible');
     cy.get('@home-link').should('be.visible');
-    cy.get('@home-link').invoke('href').should('exist');
-    cy.get('@home-link').invoke('href').should('be', '/');
-    cy.get('@home-link').click();
+    cy.get('@home-link').should('have.attr', 'href', '/');
   });
 
   it('renders a create text button link.', () => {
     cy.mount(<NavBar/>);
-    cy.get('[data-test="create"]').as('create-button');
+    cy.get('#create').as('create-button');
     cy.get('@create-button').should('be.visible');
     cy.get('@create-button').contains('Create');
-    cy.get('@create-button').invoke('href').should('exist');
-    cy.get('@create-button').invoke('href').should('be', '/create');
-    cy.get('@create-button').click();
+    cy.get('@create-button').should('have.attr', 'href', '/create');
   });
 
   it('renders a discover text button link.', () => {
     cy.mount(<NavBar/>);
-    cy.get('[data-test="discover"]').as('discover-button');
+    cy.get('#discover').as('discover-button');
     cy.get('@discover-button').should('be.visible');
     cy.get('@discover-button').contains('Discover');
-    cy.get('@discover-button').invoke('href').should('exist');
-    cy.get('@discover-button').invoke('href').should('be', '/discover');
-    cy.get('@discover-button').click();
+    cy.get('@discover-button').should('have.attr', 'href', '/discover');
   });
 
   it('renders a login outlined button link.', () => {
     cy.mount(<NavBar/>);
-    cy.get('[data-test="login"]').as('login-button');
-    cy.get('@login-button').should('be.visible');
-    cy.get('@login-button').contains('Login');
-    cy.get('@login-button').invoke('href').should('exist');
-    cy.get('@login-button').invoke('href').should('be', '/account/login');
-    cy.get('@login-button').click();
+    cy.get('#signin').as('signin-button');
+    cy.get('@signin-button').should('be.visible');
+    cy.get('@signin-button').contains('Sign In');
+    cy.get('@signin-button').should('have.attr', 'href', '/account/signin');
   });
 
   it('renders a join now filled button link.', () => {
     cy.mount(<NavBar/>);
-    cy.get('[data-test="join-now"]').as('join-now-button');
+    cy.get('#join-now').as('join-now-button');
     cy.get('@join-now-button').should('be.visible');
     cy.get('@join-now-button').contains('Join Now');
-    cy.get('@join-now-button').invoke('href').should('exist');
-    cy.get('@join-now-button').invoke('href').should('be', '/account/create');
-    cy.get('@join-now-button').click();
+    cy.get('@join-now-button').should('have.attr', 'href', '/account/create');
   });
 })
