a3c35c530b5875ecc7dfac12618a1f875b02ba75
Wesley Mui <68392398+wesleymui@users.noreply.github.com>
2023-11-14 00:59:31 -0500

    Make forgot-password page a module

--- client/src/app/account/forgot-password/page.tsx
+++ client/src/app/account/forgot-password/page.tsx
@@ -6,13 +6,14 @@ import styles from './ui/forgotUsername.module.css';
 
 function Page() {
   return (
-    <div className={styles.container}>
-      {/* <Typography className={styles.body} variant="body1" align="left">
-        Join MapHub to edit maps in any way you can imagine. Get access to
-        liking, commenting, and sharing others' maps.
-      </Typography> */}
-      {/* <ForgotUsernameForm /> */}
-    </div>
+    // <div className={styles.container}>
+    //   {/* <Typography className={styles.body} variant="body1" align="left">
+    //     Join MapHub to edit maps in any way you can imagine. Get access to
+    //     liking, commenting, and sharing others' maps.
+    //   </Typography> */}
+    //   {/* <ForgotUsernameForm /> */}
+    // </div>
+    <div></div>
   );
 }
 
