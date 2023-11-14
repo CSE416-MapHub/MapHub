f99c5b644b1df8f2e4c4089eb73bea2584f3038e
ALLECHEN <43279583+PogChan@users.noreply.github.com>
2023-11-14 01:58:27 -0500

    created the newSymbol modal

--- client/src/app/create/ui/components/PropertySVGInput.tsx
+++ client/src/app/create/ui/components/PropertySVGInput.tsx
@@ -1,43 +1,62 @@
-"use client"
+'use client';
 
-import { IconButton, Paper, Popover } from "@mui/material";
-import { useState } from "react";
+import { IconButton, Paper, Popover } from '@mui/material';
+import { useState } from 'react';
 import InterestsIcon from '@mui/icons-material/Interests';
-import styles from "./Property.module.scss"
-import Button from "components/button";
+import styles from './Property.module.scss';
+import Button from 'components/button';
+import NewSymbolModal from './modals/newSymbolModal';
 
+export default function () {
+  const [anchorEl, setAnchorEl] = useState<HTMLElement | null>(null);
+  const [openSymbolModal, setOpenSymbolModal] = useState(false);
 
-export default function() {
-    const [anchorEl, setAnchorEl] = useState<HTMLElement |null>(null)
-
-    return <>
-        <IconButton onClick={(e)=>{setAnchorEl(e.currentTarget)}}>
-            <InterestsIcon />
-        </IconButton>
-        <Popover
-            open={anchorEl!==null}
-            anchorEl={anchorEl}
-            anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'left',
-            }}
-            onClose={()=>{setAnchorEl(null)}}
+  function onConfirmSymbolModal() {}
+  return (
+    <>
+      <IconButton
+        onClick={e => {
+          setAnchorEl(e.currentTarget);
+        }}
+      >
+        <InterestsIcon />
+      </IconButton>
+      <Popover
+        open={anchorEl !== null}
+        anchorEl={anchorEl}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'left',
+        }}
+        onClose={() => {
+          setAnchorEl(null);
+        }}
+      >
+        <Paper
+          sx={{
+            width: '196px',
+            height: '230px',
+            borderRadius: '8px',
+            textAlign: 'center',
+          }}
         >
-            <Paper sx={{
-                width: "196px",
-                height: "230px",
-                borderRadius: "8px",
-                textAlign: "center"
-            }}>
-                <div className={styles["svg-grid-container"]}>
-                    <div className={styles["svg-grid"]}>
-                        {new Array(36).fill(0).map(() => <div className={styles["svg-grid-item"]}></div>)}
-                    </div>
-                </div>
-                <Button variant="filled">
-                    + New Symbol
-                </Button>
-            </Paper>
-        </Popover>
+          <div className={styles['svg-grid-container']}>
+            <div className={styles['svg-grid']}>
+              {new Array(36).fill(0).map(() => (
+                <div className={styles['svg-grid-item']}></div>
+              ))}
+            </div>
+          </div>
+          <Button variant="filled" onClick={() => setOpenSymbolModal(true)}>
+            + New Symbol
+          </Button>
+        </Paper>
+      </Popover>
+      <NewSymbolModal
+        open={openSymbolModal}
+        onClose={() => setOpenSymbolModal(false)}
+        onConfirm={onConfirmSymbolModal}
+      />
     </>
+  );
 }
