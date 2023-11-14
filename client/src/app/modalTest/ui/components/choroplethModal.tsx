48659b3f5786cb3711cf8697c159546c5b448a04
ALLECHEN <43279583+PogChan@users.noreply.github.com>
2023-11-13 20:50:12 -0500

    added choropleth modal

---
+++ client/src/app/modalTest/ui/components/choroplethModal.tsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import { TextField } from '@mui/material';
+import GeneralizedDialog from 'components/modals/GeneralizedDialog';
+import LabelSelector from './LabelSelector'; // Adjust the import path as needed
+
+interface ImportModalProps {
+  open: boolean;
+  onClose: () => void;
+  onConfirm: (mapName: string, selectedOptions: string[]) => void; // Added mapName to the callback
+  properties: string[];
+}
+
+const ImportModal: React.FC<ImportModalProps> = ({
+  open,
+  onClose,
+  onConfirm,
+  properties,
+}) => {
+  const [mapName, setMapName] = useState('');
+  const [selectedOptions, setSelectedOptions] = useState<string[]>([]);
+
+  const handleSelectionChange = (selection: string[]) => {
+    setSelectedOptions(selection);
+  };
+
+  const handleConfirm = () => {
+    onConfirm(mapName, selectedOptions);
+    onClose();
+  };
+
+  return (
+    <GeneralizedDialog
+      open={open}
+      onClose={onClose}
+      onConfirm={handleConfirm}
+      title="Choropleth Properties"
+    >
+      <LabelSelector
+        properties={properties}
+        isCheckbox={false}
+        onSelect={handleSelectionChange}
+      />
+    </GeneralizedDialog>
+  );
+};
+
+export default ImportModal;
