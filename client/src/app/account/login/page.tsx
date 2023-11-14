4965cf3fd3f25360645c1f75d64214dfcdc92593
Wesley Mui <68392398+wesleymui@users.noreply.github.com>
2023-11-13 19:37:37 -0500

    Move title into LoginForm component

--- client/src/app/account/login/page.tsx
+++ client/src/app/account/login/page.tsx
@@ -7,9 +7,6 @@ import LoginForm from './ui/components/loginForm';
 function Page() {
   return (
     <div className={styles.container}>
-      <Typography className={styles.title} variant="h2" align="center">
-        Login
-      </Typography>
       {/* <Typography className={styles.body} variant="body1" align="left">
         Join MapHub to edit maps in any way you can imagine. Get access to
         liking, commenting, and sharing others' maps.
