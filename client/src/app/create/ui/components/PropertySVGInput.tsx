661faf5f45c4d9dfa491363de41e8b510951d6ae
William Gao <william.gao09@gmail.com>
2023-11-14 01:56:54 -0500

    Added keys to components

--- client/src/app/create/ui/components/PropertySVGInput.tsx
+++ client/src/app/create/ui/components/PropertySVGInput.tsx
@@ -1,43 +1,52 @@
-"use client"
+'use client';
 
-import { IconButton, Paper, Popover } from "@mui/material";
-import { useState } from "react";
+import { IconButton, Paper, Popover } from '@mui/material';
+import { useState } from 'react';
 import InterestsIcon from '@mui/icons-material/Interests';
-import styles from "./Property.module.scss"
-import Button from "components/button";
+import styles from './Property.module.scss';
+import Button from 'components/button';
 
+export default function () {
+  const [anchorEl, setAnchorEl] = useState<HTMLElement | null>(null);
 
-export default function() {
-    const [anchorEl, setAnchorEl] = useState<HTMLElement |null>(null)
-
-    return <>
-        <IconButton onClick={(e)=>{setAnchorEl(e.currentTarget)}}>
-            <InterestsIcon />
-        </IconButton>
-        <Popover
-            open={anchorEl!==null}
-            anchorEl={anchorEl}
-            anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'left',
-            }}
-            onClose={()=>{setAnchorEl(null)}}
+  return (
+    <>
+      <IconButton
+        onClick={e => {
+          setAnchorEl(e.currentTarget);
+        }}
+      >
+        <InterestsIcon />
+      </IconButton>
+      <Popover
+        open={anchorEl !== null}
+        anchorEl={anchorEl}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'left',
+        }}
+        onClose={() => {
+          setAnchorEl(null);
+        }}
+      >
+        <Paper
+          sx={{
+            width: '196px',
+            height: '230px',
+            borderRadius: '8px',
+            textAlign: 'center',
+          }}
         >
-            <Paper sx={{
-                width: "196px",
-                height: "230px",
-                borderRadius: "8px",
-                textAlign: "center"
-            }}>
-                <div className={styles["svg-grid-container"]}>
-                    <div className={styles["svg-grid"]}>
-                        {new Array(36).fill(0).map(() => <div className={styles["svg-grid-item"]}></div>)}
-                    </div>
-                </div>
-                <Button variant="filled">
-                    + New Symbol
-                </Button>
-            </Paper>
-        </Popover>
+          <div className={styles['svg-grid-container']}>
+            <div className={styles['svg-grid']}>
+              {new Array(36).fill(0).map((_, i) => (
+                <div key={i} className={styles['svg-grid-item']}></div>
+              ))}
+            </div>
+          </div>
+          <Button variant="filled">+ New Symbol</Button>
+        </Paper>
+      </Popover>
     </>
+  );
 }
