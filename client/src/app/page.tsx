5f719f6e66af6263a96b292ad886d7459fc33424
ALLECHEN <43279583+PogChan@users.noreply.github.com>
2023-11-15 11:27:39 -0500

    added random text per Ta instructions

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -8,36 +8,33 @@ import styles from './styles/home.module.scss';
 
 function Home() {
   return (
-    <main id="home-hero" className="flex min-h-screen flex-col items-center justify-center p-24">
-      <Box
-        id="hero-display" className={styles.hero__box}
-      >
+    <main
+      id="home-hero"
+      className="flex min-h-screen flex-col items-center justify-center p-24"
+    >
+      <Box id="hero-display" className={styles.hero__box}>
         <Container id="hero-body" className={styles.hero__contentbox}>
-          <Typography variant="h1">
-            A Complete
-            Map Visuals Studio.
-          </Typography>
-          <Typography variant='body1' sx={{marginTop: '10px'}}>
-            Five Essential Templates.
-            A Plethora of Editing Tools.
-            Unique data-driven approach.
+          <Typography variant="h1">A Complete Map Visuals Studio.</Typography>
+          <Typography variant="body1" sx={{ marginTop: '10px' }}>
+            Five Essential Templates. hfghtswythsrrjytyhryrtg A Plethora of
+            Editing Tools. Unique data-driven approach.
           </Typography>
         </Container>
-        <Container sx={{
-          alignItems: 'center',
-          justifyContent: 'center',
-          mx: 'auto',
-          marginLeft: '50px'
-        }}>
+        <Container
+          sx={{
+            alignItems: 'center',
+            justifyContent: 'center',
+            mx: 'auto',
+            marginLeft: '50px',
+          }}
+        >
           <Link id="hero-discover" href="/discover">
             <Button variant="outlined" sx={{ marginRight: '16px' }}>
-                Discover Maps
-              </Button>
-            </Link>
-          <Link id="hero-create" href="/create">
-          <Button variant="filled">
-              Start Creating
+              Discover Maps
             </Button>
+          </Link>
+          <Link id="hero-create" href="/create">
+            <Button variant="filled">Start Creating</Button>
           </Link>
         </Container>
       </Box>
