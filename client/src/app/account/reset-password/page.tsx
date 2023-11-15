1befb093a8deb24ead5ee34b476c62ff707cf1db
Wesley Mui <68392398+wesleymui@users.noreply.github.com>
2023-11-15 01:42:42 -0500

    Add reset password page

---
+++ client/src/app/account/reset-password/page.tsx
@@ -0,0 +1,21 @@
+'use client'
+
+import Link from 'next/link';
+import { Typography } from '@mui/material';
+
+import styles from './ui/resetPassword.module.css';
+import ResetPasswordForm from './ui/components/resetPasswordForm';
+
+function Page() {
+  return (
+    <div className={styles.container}>
+      {/* <Typography className={styles.body} variant="body1" align="left">
+        Join MapHub to edit maps in any way you can imagine. Get access to
+        liking, commenting, and sharing others' maps.
+      </Typography> */}
+      <ResetPasswordForm />
+    </div>
+  );
+}
+
+export default Page;
