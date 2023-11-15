3f86637c63d13f6779dea4f63c4ef1137e0e4fa3
ALLECHEN <43279583+PogChan@users.noreply.github.com>
2023-11-15 11:27:39 -0500

    added random text per Ta instructions

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -8,29 +8,30 @@ import styles from './styles/home.module.scss';
 
 function Home() {
   return (
-    <main id="home-hero">
-      <Box id="hero-display" className={styles['hero__box']}>
-        <Container
-          id="hero-body"
-          className={`
-            ${styles['hero__container']}
-            ${styles['hero__container--copy']}
-          `}
-        >
+    <main
+      id="home-hero"
+      className="flex min-h-screen flex-col items-center justify-center p-24"
+    >
+      <Box id="hero-display" className={styles.hero__box}>
+        <Container id="hero-body" className={styles.hero__contentbox}>
           <Typography variant="h1">A Complete Map Visuals Studio.</Typography>
-          <Typography variant="body1">
-            Five Essential Templates. A Plethora of Editing Tools. Unique
-            data-driven approach.
+          <Typography variant="body1" sx={{ marginTop: '10px' }}>
+            Five Essential Templates. hfghtswythsrrjytyhryrtg A Plethora of
+            Editing Tools. Unique data-driven approach.
           </Typography>
         </Container>
         <Container
-          className={`
-            ${styles['hero__container']}
-            ${styles['hero__container--buttons']}
-          `}
+          sx={{
+            alignItems: 'center',
+            justifyContent: 'center',
+            mx: 'auto',
+            marginLeft: '50px',
+          }}
         >
           <Link id="hero-discover" href="/discover">
-            <Button variant="outlined">Discover Maps</Button>
+            <Button variant="outlined" sx={{ marginRight: '16px' }}>
+              Discover Maps
+            </Button>
           </Link>
           <Link id="hero-create" href="/create">
             <Button variant="filled">Start Creating</Button>
