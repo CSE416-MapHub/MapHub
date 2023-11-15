f711b7c54fd68f31bb0372666aebf8f4077722ca
ALLECHEN <43279583+PogChan@users.noreply.github.com>
2023-11-14 22:06:51 -0500

    changed the tests to  that it passes p -> h2

--- client/cypress/e2e/modals/editorToolBarModalOpen.cy.ts
+++ client/cypress/e2e/modals/editorToolBarModalOpen.cy.ts
@@ -4,7 +4,7 @@ describe('All Editor Toolbar Modal Opens', () => {
     cy.get('p:contains("File")').click();
     cy.get('span:contains("Import")').click();
     cy.get('span:contains("Import File From Local Desktop")').click();
-    cy.get('p:contains("Import Properties")');
+    cy.get('h2:contains("Import Properties")');
   });
 
   it('recent maps modal open', () => {
@@ -12,27 +12,27 @@ describe('All Editor Toolbar Modal Opens', () => {
     cy.get('p:contains("File")').click();
     cy.get('span:contains("Import")').click();
     cy.get('span:contains("Import User Owned Maps")').click();
-    cy.get('p:contains("Recent Maps")');
+    cy.get('h2:contains("Recent Maps")');
   });
 
   it('Map Label Multi-Selector modal open', () => {
     cy.visit('/create');
     cy.get('p:contains("View")').click();
     cy.get('span:contains("Map Label Multi Select")').click();
-    cy.get('p:contains("Map Label Multi-Selector")');
+    cy.get('h2:contains("Map Label Multi-Selector")');
   });
 
   it('Choropleth Multi-Selector modal open', () => {
     cy.visit('/create');
     cy.get('p:contains("View")').click();
     cy.get('span:contains("Choropleth Label Select")').click();
-    cy.get('p:contains("Choropleth Label Select")');
+    cy.get('h2:contains("Choropleth Label Select")');
   });
 
   it('Publish modal open', () => {
     cy.visit('/create');
     cy.get('p:contains("File")').click();
     cy.get('span:contains("Publish")').click();
-    cy.get('p:contains("Publish Map")');
+    cy.get('h2:contains("Publish Map")');
   });
 });
--- client/cypress/e2e/modals/sideBarModal.cy.ts
+++ client/cypress/e2e/modals/sideBarModal.cy.ts
@@ -3,23 +3,23 @@ describe('All Side Bar Properties Modal Opens', () => {
     cy.visit('/create');
     cy.get('#dotPopover').click();
     cy.get('span:contains("+ New Dot Type")').click();
-    cy.get('p:contains("Create New Dot Type")');
+    cy.get('h2:contains("Create New Dot Type")');
   });
   it('create new symbol open', () => {
     cy.visit('/create');
     cy.get('#symbolPopover').click();
     cy.get('button:contains("+ New Symbol")').click();
-    cy.get('p:contains("Create New Symbol")');
+    cy.get('h2:contains("Create New Symbol")');
   });
   it('create new category open', () => {
     cy.visit('/create');
     cy.get('#demo-simple-select').click();
     cy.get('li:contains("+ New Category")').click();
-    cy.get('p:contains("Create New Category")');
+    cy.get('h2:contains("Create New Category")');
   });
   it('delete category modal open', () => {
     cy.visit('/create');
     cy.get('button:contains("Delete Category")').click();
-    cy.get('p:contains("Delete Category")');
+    cy.get('h2:contains("Delete Category")');
   });
 });
