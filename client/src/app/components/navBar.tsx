ed9d211c9931dc487aa6f1f323d4bc5cd1d5c410
Edward Ng <edwardlng@outlook.com>
2023-11-15 10:21:29 -0500

    Added a compact variant to the navbar for /create.

--- client/src/app/components/navBar.tsx
+++ client/src/app/components/navBar.tsx
@@ -1,5 +1,9 @@
+'use client';
+
+import clsx from 'clsx';
 import Image from 'next/image';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 import Button from '../../components/button';
 import styles from '../styles/navBar.module.scss';
@@ -7,7 +11,10 @@ import UserButton from './userButton';
 
 function NavBar() {
   return (
-    <nav className={styles['nav__bar']}>
+    <nav className={clsx({
+      [styles['nav__bar']]: true,
+      [styles['nav__bar--compact']]: usePathname() === '/create'
+    })}>
       <div>
         <Link id="home" href="/">
           <Image
