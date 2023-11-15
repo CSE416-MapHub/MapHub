7498fb252ec38612c8596aaa9dcc8054b76dbb57
Edward Ng <edwardlng@outlook.com>
2023-11-14 21:33:26 -0500

    Imported fonts and styling for box icons.

---
+++ client/src/app/fonts/fonts.ts
@@ -0,0 +1,36 @@
+import localFont from 'next/font/local';
+import { Fira_Sans, Sofia_Sans_Condensed } from 'next/font/google';
+
+const firaSans = Fira_Sans({
+  weight: ['400', '500', '700'],
+  subsets: ['latin'],
+  variable: '--fira',
+});
+const sofiaSansCondensed = Sofia_Sans_Condensed({
+  subsets: ['latin'],
+  variable: '--sofia',
+});
+const boxIcons = localFont({
+  src: [
+    {
+      path: './boxicons.eot',
+    },
+    {
+      path: './boxicons.woff2',
+    },
+    {
+      path: './boxicons.woff'
+    },
+    {
+      path: './boxicons.ttf',
+    },
+  ],
+  weight: '400',
+  style: 'normal',
+  declarations: [{
+    prop: 'font-family',
+    value: 'boxicons'
+  }],
+});
+
+export {firaSans, sofiaSansCondensed, boxIcons};
