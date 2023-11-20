d04f6dbafc1fcd7d2f3bdcb4d5c60a712c2d46a6
William Gao <william.gao09@gmail.com>
2023-11-20 16:22:26 -0500

    Force clicks and uploads added

--- client/cypress/e2e/editor/upload.cy.ts
+++ client/cypress/e2e/editor/upload.cy.ts
@@ -4,11 +4,16 @@ describe('Uploads', () => {
     cy.get('button:contains("File")').click();
     cy.get('span:contains("Import")').click();
     cy.get('span:contains("Import File From Local Desktop")').click();
-    cy.get('#import-file-upload-button').selectFile([
-      './cypress/fixtures/VAT_adm0.dbf',
-      './cypress/fixtures/VAT_adm0.shp',
-      './cypress/fixtures/VAT_adm0.shx',
-    ]);
+    cy.get('#import-file-upload-button').selectFile(
+      [
+        './cypress/fixtures/VAT_adm0.dbf',
+        './cypress/fixtures/VAT_adm0.shp',
+        './cypress/fixtures/VAT_adm0.shx',
+      ],
+      {
+        force: true,
+      },
+    );
     cy.get('h2:contains("Import Properties")');
   });
 
@@ -17,9 +22,12 @@ describe('Uploads', () => {
     cy.get('button:contains("File")').click();
     cy.get('span:contains("Import")').click();
     cy.get('span:contains("Import File From Local Desktop")').click();
-    cy.get('#import-file-upload-button').selectFile([
-      './cypress/fixtures/VAT_adm0.shx',
-    ]);
+    cy.get('#import-file-upload-button').selectFile(
+      ['./cypress/fixtures/VAT_adm0.shx'],
+      {
+        force: true,
+      },
+    );
     cy.get('h2:contains("Import Properties")').should('not.exist');
   });
 
@@ -28,9 +36,12 @@ describe('Uploads', () => {
     cy.get('button:contains("File")').click();
     cy.get('span:contains("Import")').click();
     cy.get('span:contains("Import File From Local Desktop")').click();
-    cy.get('#import-file-upload-button').selectFile([
-      './cypress/fixtures/VAT_adm0.geojson',
-    ]);
+    cy.get('#import-file-upload-button').selectFile(
+      ['./cypress/fixtures/VAT_adm0.geojson'],
+      {
+        force: true,
+      },
+    );
     // this is a semirandom subset of properties available
     const props = [
       'ID_0',
@@ -63,9 +74,12 @@ describe('Uploads', () => {
     cy.get('button:contains("File")').click();
     cy.get('span:contains("Import")').click();
     cy.get('span:contains("Import File From Local Desktop")').click();
-    cy.get('#import-file-upload-button').selectFile([
-      './cypress/fixtures/VAT_adm0.geojson',
-    ]);
+    cy.get('#import-file-upload-button').selectFile(
+      ['./cypress/fixtures/VAT_adm0.geojson'],
+      {
+        force: true,
+      },
+    );
     cy.get('[value="My New Map"]').type('My Cool Map Name');
     cy.get(`p:contains("Confirm")`).click();
     cy.get('h3:contains("My New MapMy Cool Map Name")');
