1b245062c2b14a1a7a9dbfef3f06f7cbd72167ea
William Gao <william.gao09@gmail.com>
2023-11-20 15:59:44 -0500

    Updated test cases

--- client/cypress/e2e/editor/propertiesPanel.cy.ts
+++ client/cypress/e2e/editor/propertiesPanel.cy.ts
@@ -1,7 +1,9 @@
 describe('Properties in the panel should change as I click', () => {
   it('should give me the name when I click on a feature', () => {
     cy.loadVatican();
-    cy.get('.leaflet-interactive').first().click();
+    cy.get('.leaflet-interactive').first().click({
+      force: true,
+    });
     cy.get('[value="HOLY SEE (VATICAN CITY STATE)"]');
   });
 });
--- client/cypress/support/e2e.ts
+++ client/cypress/support/e2e.ts
@@ -14,17 +14,26 @@ declare global {
 
 Cypress.on('log:added', logObject => console.log(logObject));
 
+const special = 'file.spss';
+
 Cypress.Commands.add('loadVatican', () => {
   cy.visit('/create');
   cy.get('button:contains("File")').click();
   cy.get('span:contains("Import")').click();
   cy.get('span:contains("Import File From Local Desktop")').click();
-  cy.get('#import-file-upload-button').attachFile([
-    'VAT_adm0.dbf',
-    'VAT_adm0.shp',
-    // 'VAT_adm0.shx',
-  ]);
-  cy.get('span:contains("NAME_ISO")');
+  cy.get('#import-file-upload-button').selectFile(
+    [
+      // { filePath: 'VAT_adm0.shp', encoding: 'utf-8' },
+      // { filePath: 'VAT_adm0.dbf', encoding: 'utf-8' },
+      './cypress/fixtures/VAT_adm0.dbf',
+      './cypress/fixtures/VAT_adm0.shp',
+      // 'VAT_adm0.shx',
+    ],
+    {
+      force: true,
+    },
+  );
+  cy.get('span:contains("NAME_ISO")').click();
   cy.get('p:contains("Confirm")').click();
 });
 
