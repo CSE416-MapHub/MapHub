567c5dc953f2cc482a8854e1374785eeb92ab5b3
Edward Ng <edwardlng@outlook.com>
2023-11-21 17:30:22 -0500

    Added navigation to the account menu.

--- client/src/app/components/navAvatar.tsx
+++ client/src/app/components/navAvatar.tsx
@@ -1,6 +1,8 @@
 'use client';
 
-import { useState } from 'react';
+import { MouseEventHandler, useState } from 'react';
+import { useRouter } from 'next/navigation';
+import AccountAPI from 'api/AccountAPI';
 import Avatar from '../../components/avatar';
 import Button from '../../components/button';
 import Icon from '../../components/icon';
@@ -12,11 +14,31 @@ import styles from '../styles/navAvatar.module.scss';
 function NavAvatar() {
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
   const open = Boolean(anchorEl);
+  const router = useRouter();
   const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
     setAnchorEl(event.currentTarget);
   };
   const handleClose = () => {
     setAnchorEl(null);
+  };
+
+  const handleDashboardClick: MouseEventHandler = () => {
+    router.push('/account/dashboard');
+  };
+
+  const handleSettingsClick: MouseEventHandler = () => {
+    router.push('/account/settings');
+  };
+
+  const handleSignOutClick: MouseEventHandler = async () => {
+    try {
+      const response = await AccountAPI.logoutUser();
+      console.log('Logout Successful: ', response);
+    } catch (error) {
+      console.log('Logout Failed: ', error);
+    } finally {
+      router.replace('/');
+    }
   };
 
   return (
@@ -42,15 +64,15 @@ function NavAvatar() {
           'aria-labelledby': 'basic-button',
         }}
       >
-        <MenuItem id="account-dashboard">
+        <MenuItem id="account-dashboard" onClick={handleDashboardClick}>
           <Icon type="solid" name="dashboard" />
           Dashboard
         </MenuItem>
-        <MenuItem id="account-settings">
+        <MenuItem id="account-settings" onClick={handleSettingsClick}>
           <Icon type="solid" name="cog" />
           Settings
         </MenuItem>
-        <MenuItem id="account-sign-out">
+        <MenuItem id="account-sign-out" onClick={handleSignOutClick}>
           <Icon type="solid" name="log-out" />
           Sign Out
         </MenuItem>
