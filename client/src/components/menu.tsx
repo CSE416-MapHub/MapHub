f0154e8eab07a774e9f30c95ae78c02040b6ff0e
Edward Ng <edwardlng@outlook.com>
2023-11-20 23:46:12 -0500

    Created menu item and menu components.

--- client/src/components/menu.tsx
+++ client/src/components/menu.tsx
@@ -3,9 +3,17 @@
 import { Menu as MaterialMenu } from '@mui/material';
 import { MenuProps } from '@mui/material';
 
+import styles from '../styles/menu.module.scss';
+
 function Menu({ children, ...props }: MenuProps) {
   return (
-    <MaterialMenu {...props}>
+    <MaterialMenu
+      classes={{
+        list: styles['menu__list'],
+        paper: styles['menu__container'],
+      }}
+      {...props}
+    >
       {children}
     </MaterialMenu>
   );
