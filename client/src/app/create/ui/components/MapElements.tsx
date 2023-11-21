d937a6d441c0b389c56dd1cbc6f6d5ab1d1cdfa5
William Gao <william.gao09@gmail.com>
2023-11-21 18:42:30 -0500

    Fixed zoom on new map load

--- client/src/app/create/ui/components/MapElements.tsx
+++ client/src/app/create/ui/components/MapElements.tsx
@@ -1,37 +1,43 @@
 'use client';
-import { EditorActions, EditorContext, ToolbarButtons, IEditorState } from 'context/EditorProvider';
+import {
+  EditorActions,
+  EditorContext,
+  ToolbarButtons,
+  IEditorState,
+} from 'context/EditorProvider';
 import * as G from 'geojson';
-import * as L from "leaflet"
+import * as L from 'leaflet';
 import { useContext, useState, useEffect, useRef } from 'react';
 import { GeoJSON, TileLayer, useMap } from 'react-leaflet';
 import { IInputProps } from './PropertyInput';
 
-import {Dispatch} from "react"
+import { Dispatch } from 'react';
 
-const OPEN_BOUNDS = L.latLngBounds(
-  L.latLng(-900, 1800),
-  L.latLng(900, -1800)
-)
+const OPEN_BOUNDS = L.latLngBounds(L.latLng(-900, 1800), L.latLng(900, -1800));
+
+const MIN_ZOOM = 0;
+const MAX_ZOOM = 20;
 
 export default function () {
   const editorContextStaleable = useContext<{
-    state: IEditorState,
+    state: IEditorState;
     dispatch: Dispatch<{
-        type: EditorActions,
-        payload: Partial<IEditorState>,
-    }>}>(EditorContext);
+      type: EditorActions;
+      payload: Partial<IEditorState>;
+    }>;
+  }>(EditorContext);
   const map = useMap();
   const [eBBox, setEBBox] = useState<[number, number]>([0, 0]);
   const [rerender, setRerender] = useState(0);
   const editorContextRef = useRef<{
-    state: IEditorState,
+    state: IEditorState;
     dispatch: Dispatch<{
-        type: EditorActions,
-        payload: Partial<IEditorState>,
-    }>}>(editorContextStaleable)
+      type: EditorActions;
+      payload: Partial<IEditorState>;
+    }>;
+  }>(editorContextStaleable);
 
-  editorContextRef.current = editorContextStaleable
-
+  editorContextRef.current = editorContextStaleable;
 
   useEffect(() => {
     let b = editorContextStaleable.state.mapDetails.bbox;
@@ -39,29 +45,35 @@ export default function () {
       let c: [number, number] = [b[1], b[0]];
       setEBBox(c);
       setRerender(rerender + 1);
+      map.setMaxZoom(MAX_ZOOM);
+      map.setMinZoom(MIN_ZOOM);
       map.setView([c[0] + b[3] / 2, c[1] + b[2] / 2], 10 - Math.log2(b[2]));
+      map.setMaxZoom(map.getZoom());
+      map.setMinZoom(map.getZoom());
     }
 
     // if the tool selected is pan, allow for panning
     if (editorContextStaleable.state.selectedTool !== ToolbarButtons.pan) {
-      map.setMaxBounds(map.getBounds())
+      map.setMaxBounds(map.getBounds());
+      map.setMaxZoom(map.getZoom());
+      map.setMinZoom(map.getZoom());
     } else {
-      // else restrict the bounds
-      
-      map.setMaxBounds(OPEN_BOUNDS)
-      // map.on('drag', function() {
-      //     map.panInsideBounds(map.getBounds(), { animate: false });
-      // });
+      map.setMaxBounds(OPEN_BOUNDS);
+      map.setMaxZoom(MAX_ZOOM);
+      map.setMinZoom(MIN_ZOOM);
     }
-    
-
   });
 
-  function perFeatureHandler(feature: G.Feature, layer:L.Layer) {
+  function perFeatureHandler(feature: G.Feature, layer: L.Layer) {
     layer.addEventListener('click', () => {
-      console.log("clicked a feature")
-      if (editorContextRef.current.state.selectedTool !==  ToolbarButtons.select) {
-        console.log("but the current tool isnt select; it is " + editorContextRef.current.state.selectedTool)
+      console.log('clicked a feature');
+      if (
+        editorContextRef.current.state.selectedTool !== ToolbarButtons.select
+      ) {
+        console.log(
+          'but the current tool isnt select; it is ' +
+            editorContextRef.current.state.selectedTool,
+        );
         return;
       }
       let action = {
@@ -84,7 +96,9 @@ export default function () {
                       type: 'text',
                       short: false,
                       disabled: false,
-                      value: feature.properties? (feature.properties[lbl]).toString() : "undefined"
+                      value: feature.properties
+                        ? feature.properties[lbl].toString()
+                        : 'undefined',
                     },
                   };
                 },
@@ -114,7 +128,6 @@ export default function () {
     <GeoJSON
       key={rerender}
       data={editorContextStaleable.state.map?.geoJSON}
-      
       onEachFeature={perFeatureHandler}
     />
   );
