1eca98aa27611bca6470f6a22beb794bff87b30f
ALLECHEN <43279583+PogChan@users.noreply.github.com>
2023-11-22 03:11:09 -0500

    mocked all functions, added all posted get

--- server/tests/post.test.ts
+++ server/tests/post.test.ts
@@ -3,13 +3,15 @@ import app from '../app';
 import postModel from '../models/post-model';
 import mongoose from 'mongoose';
 import auth from '../auth/index';
-
-const userId = new mongoose.Types.ObjectId();
+import mapModel from '../models/map-model';
+const mockUserID = new mongoose.Types.ObjectId();
 
 beforeEach(() => {
   jest.setTimeout(6000);
 });
-
+afterEach(() => {
+  jest.restoreAllMocks();
+});
 jest.mock('../models/post-model');
 
 describe('POST /posts/publish', () => {
@@ -21,12 +23,30 @@ describe('POST /posts/publish', () => {
       title: 'Ukranian War',
       description: 'very sad times',
       mapID: mapId,
-      owner: userId,
+      owner: mockUserID,
       comments: [],
       likes: [],
     };
+    const mockMap = {
+      title: 'Blah blah',
+      _id: new mongoose.Types.ObjectId(),
+      save: jest.fn(), // Add a mock save function here if needed
+    };
 
     postModel.prototype.save = jest.fn().mockResolvedValue(savedPost);
+
+    jest.spyOn(mapModel, 'findById').mockImplementation((id: any) => {
+      const queryLikeObject = {
+        exec: jest.fn().mockResolvedValue(mockMap),
+      };
+      return queryLikeObject as any;
+    });
+
+    jest
+      .spyOn(mapModel.prototype, 'save')
+      .mockImplementation(function (this: any) {
+        return Promise.resolve(this);
+      });
 
     const response = await supertest(app)
       .post(`/posts/publish/`)
@@ -35,10 +55,77 @@ describe('POST /posts/publish', () => {
         title: savedPost.title,
         description: savedPost.description,
       })
-      .set('Cookie', [`token=${auth.signToken(userId.toString())}`]);
+      .set('Cookie', [`token=${auth.signToken(mockUserID.toString())}`]);
 
     expect(response.statusCode).toBe(200);
     expect(response.body).toHaveProperty('post');
     expect(response.body.post).toEqual({ postId: mockId.toString() });
   });
+});
+
+describe('GET /posts/all', () => {
+  it('queries posts based on a search term', async () => {
+    const mockPosts = [
+      {
+        _id: new mongoose.Types.ObjectId(),
+        title: 'aLL long in War ku dynasty',
+        description: 'Description for Post 1',
+        map: new mongoose.Types.ObjectId(),
+        owner: mockUserID,
+        comments: [new mongoose.Types.ObjectId()], // Array of comment ObjectIds
+        likes: [mockUserID], // Array of user ObjectIds who liked the post
+      },
+      {
+        _id: new mongoose.Types.ObjectId(),
+        title: 'han dynasty',
+        description: 'Description for Post 2',
+        map: new mongoose.Types.ObjectId(),
+        owner: mockUserID,
+        comments: [], // Assuming no comments
+        likes: [], // Assuming no likes
+      },
+    ];
+
+    const returnMock = [
+      {
+        title: mockPosts[0].title,
+        description: mockPosts[0].description,
+        postID: mockPosts[0]._id,
+        mapID: mockPosts[0].map,
+        png: { type: 'Buffer', data: [] },
+      },
+    ];
+
+    const mapData = {
+      _id: new mongoose.Types.ObjectId(),
+    };
+
+    const queryMock: any = {
+      exec: jest.fn().mockResolvedValue([mockPosts[0]]),
+    };
+
+    jest.spyOn(postModel, 'find').mockImplementation(() => queryMock);
+
+    jest.spyOn(mapModel, 'findById').mockImplementation((id: any) => {
+      const queryLikeObject = {
+        exec: jest.fn().mockResolvedValue(mapData),
+      };
+      return queryLikeObject as any;
+    });
+
+    // Make the GET request
+    const searchQuery = 'dynasty';
+    const response = await supertest(app)
+      .get(`/posts/all?searchQuery=${searchQuery}`)
+      .set('Cookie', [`token=${auth.signToken(mockUserID.toString())}`]);
+
+    // Assertions
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toHaveProperty('success', true);
+    expect(response.body).toHaveProperty('posts');
+
+    expect(response.body.posts[0].title).toEqual(returnMock[0].title);
+  });
+
+  // Additional test cases, e.g., empty search query, no posts found, etc.
 });
