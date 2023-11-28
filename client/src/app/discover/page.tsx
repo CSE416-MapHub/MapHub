bb0f605ca7d4cb568e47194228e25e5543656bbd
Wesley Mui <68392398+wesleymui@users.noreply.github.com>
2023-11-28 02:50:04 -0500

    user can filter maps by name

--- client/src/app/discover/page.tsx
+++ client/src/app/discover/page.tsx
@@ -8,22 +8,35 @@ import PostAPI from 'api/PostAPI';
 
 export default function () {
   const [mapCardData, setMapCardData] = useState<MapCardProps[]>([]);
-  
+  const [searchResponse, setSearchResponse] = useState<any>(null);
+
   useEffect(() => {
-    PostAPI.queryPosts('')
+    // Fetch initial set of maps when the component mounts
+    fetchMapCardData('');
+  }, []);
+
+  const fetchMapCardData = (searchValue: string) => {
+    // Fetch map card data based on search value
+    PostAPI.queryPosts(searchValue)
       .then((response) => {
-        if(response.data.success) {
+        if (response.data.success) {
           setMapCardData(response.data.posts);
+          setSearchResponse(response); // Set the response data to pass to SearchBar
         }
       })
-      .catch(error => {
-        console.error('Error while querying posts:', error);
-      })
-  }, []);
+      .catch((error) => {
+        console.error('Error while querying or searching posts:', error);
+      });
+  };
+
+  const handleSearch = (searchValue: string) => {
+    // Fetch map card data based on search value
+    fetchMapCardData(searchValue);
+  };
 
   return (
     <>
-      <SearchBar />
+      <SearchBar onSearch={handleSearch} searchResponse={searchResponse} />
       <div className={style['card-grid']}>
         {mapCardData.map(map => (
           <MapCard
--- client/src/app/discover/ui/components/SearchBar.tsx
+++ client/src/app/discover/ui/components/SearchBar.tsx
@@ -15,33 +15,37 @@ import {
 import StarOutlineIcon from '@mui/icons-material/StarOutline';
 import PostAPI from 'api/PostAPI';
 
-export default function () {
+interface SearchBarProps {
+  onSearch: (searchValue: string) => void;
+  searchResponse: any;
+}
+
+const SearchBar: React.FC<SearchBarProps> = ({ onSearch, searchResponse }) => {
   const [searchBar, setSearchBar] = useState<string>('');
-  const [searchSuggestions, setSearchSuggestions] = useState<
-    [string, string][]
-  >([
+
+  const [searchSuggestions, setSearchSuggestions] = useState<[string, string][]>([
     ['113463640', 'January Arctic Fox Migrations in 2008'],
     ['114435342', 'Jumping Bug Populations in Brazil'],
     ['112345674', 'Jeromes Born in India'],
   ]);
-
-  const handleUpdateSearch = (e : any) => {
-    setSearchBar(e.target.value);
-    PostAPI.queryPosts(e.target.value)
-      .then(response => {
-        console.log(response);
-      })
-      .catch(error => {
-        console.log('Searching failed', error);
-      })
-  }
 
   const [people, setPeople] = useState<[string, string][]>([
     ['1', 'Juma'],
     ['2', 'Fatima'],
     ['3', 'Irenee'],
   ]);
+  const handleUpdateSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const searchValue = e.target.value;
+    // Call the onSearch function when the search input changes
+    setSearchBar(searchValue);
+    onSearch(searchValue);
+  };
+
+  // Use the searchResponse data as needed
+  console.log('Search Response:', searchResponse);
+
   return (
+    // Your SearchBar component JSX
     <>
       <div className={style['search-container']}>
         <div
@@ -111,4 +115,104 @@ export default function () {
       </div>
     </>
   );
-}
+};
+
+export default SearchBar;
+// export default function () {
+//   const [searchBar, setSearchBar] = useState<string>('');
+//   const [searchSuggestions, setSearchSuggestions] = useState<
+//     [string, string][]
+//   >([
+//     ['113463640', 'January Arctic Fox Migrations in 2008'],
+//     ['114435342', 'Jumping Bug Populations in Brazil'],
+//     ['112345674', 'Jeromes Born in India'],
+//   ]);
+
+//   const handleUpdateSearch = (e : any) => {
+//     setSearchBar(e.target.value);
+//     PostAPI.queryPosts(e.target.value)
+//       .then(response => {
+//         console.log(response);
+//       })
+//       .catch(error => {
+//         console.log('Searching failed', error);
+//       })
+//   }
+
+//   const [people, setPeople] = useState<[string, string][]>([
+//     ['1', 'Juma'],
+//     ['2', 'Fatima'],
+//     ['3', 'Irenee'],
+//   ]);
+//   return (
+//     <>
+//       <div className={style['search-container']}>
+//         <div
+//           style={{
+//             display: 'flex',
+//             flexDirection: 'row',
+//             gap: '4px',
+//             zIndex: 2,
+//           }}
+//         >
+//           <SearchIcon />
+//           <input
+//             type="text"
+//             className={style['search-input']}
+//             value={searchBar}
+//             onChange={handleUpdateSearch}
+//             placeholder="Search for map inspiration"
+//           />
+//         </div>
+//         <div
+//           style={{
+//             visibility: searchBar === '' ? 'collapse' : 'visible',
+//           }}
+//           className={style['search-suggestions']}
+//         >
+//           <Divider />
+//           {searchSuggestions.map(([id, name]) => (
+//             <MenuItem key={id}>
+//               <ListItemIcon>
+//                 <StarOutlineIcon fontSize="small" />
+//               </ListItemIcon>
+//               <ListItemText>{name}</ListItemText>
+//             </MenuItem>
+//           ))}
+//           <Divider />
+//           <div>
+//             <Typography>People</Typography>
+//           </div>
+//           <div
+//             style={{
+//               display: 'flex',
+//               flexDirection: 'row',
+//               gap: '4px',
+//             }}
+//           >
+//             {people.map(([id, name]) => (
+//               <div
+//                 key={id}
+//                 style={{
+//                   display: 'flex',
+//                   flexDirection: 'column',
+//                   textAlign: 'center',
+//                 }}
+//               >
+//                 {/* TODO: stock photos */}
+//                 <AccountCircleIcon
+//                   sx={{
+//                     marginLeft: 'auto',
+//                     marginRight: 'auto',
+//                   }}
+//                 />
+//                 <Typography>{name}</Typography>
+//               </div>
+//             ))}
+//           </div>
+//         </div>
+//       </div>
+//     </>
+//   );
+// }
+
