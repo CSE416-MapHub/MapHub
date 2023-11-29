8b21fa0719e3c05fc55d0285be8869fb733f1c1c
ALLECHEN <43279583+PogChan@users.noreply.github.com>
2023-11-29 02:03:18 -0500

    added on moer etest with mapid not added

--- server/tests/map.test.ts
+++ server/tests/map.test.ts
@@ -674,6 +674,19 @@ describe('/map/map update map by id', () => {
     expect(response.body).toHaveProperty('map');
     expect(response.body.map.title).toEqual(mapPayload.title);
   });
+  it('update title of map', async () => {
+    const mapPayload = { title: 'UPDATED MAP NEW BIGN' };
+
+    jest.spyOn(mapModel, 'findById').mockResolvedValue(mapData);
+
+    const response = await supertest(app)
+      .put('/map/map')
+      .send(mapPayload)
+      .set('Cookie', [`token=${auth.signToken(userId.toString())}`]);
+
+    expect(response.statusCode).toBe(400);
+    expect(response.body.message).toBe('Map ID is required');
+  });
 });
 
 afterEach(() => {
