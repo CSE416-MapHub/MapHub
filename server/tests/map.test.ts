17931d8997593aab6e1677590e438133defd9275
ALLECHEN <43279583+PogChan@users.noreply.github.com>
2023-11-28 21:06:19 -0500

    added dot create update delete handlers

--- server/tests/map.test.ts
+++ server/tests/map.test.ts
@@ -330,6 +330,13 @@ describe('/map/payload/ dot payload', () => {
       },
     };
 
+    jest
+      .spyOn(mapModel.prototype, 'save')
+      .mockImplementation(async function (this: MapDocument) {
+        console.log('MOCKING THIS?', this.title);
+        return this.toObject();
+      });
+
     jest.spyOn(mapModel, 'findById').mockResolvedValue(mockMap);
 
     const response = await supertest(app)
