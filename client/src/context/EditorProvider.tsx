0148819a353646bbbfe298c872ccdbebe1d64c5b
William Gao <william.gao09@gmail.com>
2023-11-28 21:18:13 -0500

    Integrated with backend

--- client/src/context/EditorProvider.tsx
+++ client/src/context/EditorProvider.tsx
@@ -10,6 +10,7 @@ import {
   applyDelta,
   updatePropertiesPanel,
 } from './editorHelpers/DeltaUtil';
+import MapAPI from 'api/MapAPI';
 
 export enum ToolbarButtons {
   select = 'select',
@@ -178,6 +179,7 @@ class helpers {
       });
       let nMap = { ...map };
       applyDelta(nMap, d);
+      MapAPI.updateMapPayload(d);
       let li = ctx.state.lastInstantiated;
       if (d.type === DeltaType.CREATE && d.payload.name !== undefined) {
         li = d.payload.name;
@@ -207,6 +209,7 @@ class helpers {
       // apply it to a copy of the map
       let nMap = { ...map };
       applyDelta(nMap, a.undo);
+      MapAPI.updateMapPayload(a.do);
       // create a copy of the stack with the change
       let nStack = ctx.state.actionStack.clone();
       nStack.counterStack.push(nStack.stack.pop()!);
@@ -237,6 +240,7 @@ class helpers {
       // apply it to a copy of the map
       let nMap = { ...map };
       applyDelta(nMap, a.do);
+      MapAPI.updateMapPayload(a.do);
       // create a copy of the stack with the change
       let nStack = ctx.state.actionStack.clone();
       nStack.stack.push(nStack.counterStack.pop()!);
