3ff760833653ca2c4cf493446f9666ea308e3b06
Edward Ng <edwardlng@outlook.com>
2023-12-01 14:24:48 -0500

    Modified the NotificationContext to hold a queue of notifications with a reducer.

---
+++ client/src/context/notificationsProvider.tsx
@@ -0,0 +1,65 @@
+'use client';
+
+import { createContext, useReducer, Dispatch, PropsWithChildren } from 'react';
+import { SnackbarActions } from '../components/snackbar';
+
+interface Notification {
+  key: string;
+  message: string;
+  // The set of actions to render to the snackbar.
+  actions?: SnackbarActions;
+  // The number of ms to show the notification.
+  autoHideDuration?: number;
+}
+
+enum NotificationsActionType {
+  enqueue = 'enqueue',
+  dequeue = 'dequeue',
+}
+
+type NotificationsAction =
+  | { type: NotificationsActionType.enqueue; value: Omit<Notification, 'key'> }
+  | { type: NotificationsActionType.dequeue };
+
+const NotificationsContext = createContext<{
+  state: Notification[];
+  dispatch: Dispatch<NotificationsAction>;
+}>({ state: [], dispatch: () => {} });
+
+function NotificationsProvider({ children }: PropsWithChildren) {
+  const notificationsReducer = (
+    state: Notification[],
+    action: NotificationsAction,
+  ): Notification[] => {
+    switch (action.type) {
+      case NotificationsActionType.enqueue:
+        const notification = {
+          ...action.value,
+          key: crypto.randomUUID(),
+        };
+        return [...state, notification];
+      case NotificationsActionType.dequeue:
+        const [_, ...rest] = state;
+        return rest;
+      default:
+        return state;
+    }
+  };
+
+  const [notifications, dispatchNotifications] = useReducer(
+    notificationsReducer,
+    [],
+  );
+
+  return (
+    <NotificationsContext.Provider
+      value={{ state: notifications, dispatch: dispatchNotifications }}
+    >
+      {children}
+    </NotificationsContext.Provider>
+  );
+}
+
+export type { Notification };
+export { NotificationsContext, NotificationsActionType };
+export default NotificationsProvider;
