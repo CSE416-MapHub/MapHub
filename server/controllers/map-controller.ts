f7fb325eb0a2d38b58f7acab6416c9b566adcf23
ALLECHEN <43279583+PogChan@users.noreply.github.com>
2023-12-03 15:52:11 -0500

    getRecentMaps changed to unpublished maps

--- server/controllers/map-controller.ts
+++ server/controllers/map-controller.ts
@@ -335,7 +335,7 @@ const MapController = {
       }
 
       // Retrieve the most recent maps for the user
-      let maps = await Map.find({ owner: userId })
+      let maps = await Map.find({ owner: userId, published: false })
         .sort({ updatedAt: -1 })
         .exec();
 
