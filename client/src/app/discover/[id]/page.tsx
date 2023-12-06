636a600dcee6bcc69283fb56a50592fcd65c0979
Edward Ng <edwardlng@outlook.com>
2023-12-06 02:45:15 -0500

    Refactored post page and added new general layout.

--- client/src/app/discover/[id]/page.tsx
+++ client/src/app/discover/[id]/page.tsx
@@ -1,16 +1,43 @@
-import style from './page.module.scss';
-import AuthorData from './ui/components/AuthorData';
-import Social from './ui/components/Social';
+import { notFound } from 'next/navigation';
 
-const placeholderImage =
-  'https://www.react-simple-maps.io/images/basic-world-map-md.png';
+import styles from './styles/post.module.scss';
 
-export default function Page({ params }: { params: { id: string } }) {
-  //   return <div>My Post: {params.id}</div>;
+import 'dotenv/config';
+const baseURL = process.env.API_URL
+  ? process.env.API_URL
+  : 'https://api.maphub.pro';
+
+async function fetchData(id: string) {
+  try {
+    const post = await fetch(`${baseURL}/posts/post/${id}`, {
+      method: 'GET',
+      cache: 'no-store',
+    });
+
+    if (!post.ok) {
+      return undefined;
+    }
+
+    const body: { success: boolean; post: any } = await post.json();
+    return body.post;
+  } catch (error) {
+    return undefined;
+  }
+}
+
+async function Post({ params }: { params: { id: string } }) {
+  const post = await fetchData(params.id);
+
+  if (!post) {
+    notFound();
+  }
+
   return (
-    <main id={style['map-post']}>
-      <img className={style['map-container']} src={placeholderImage}></img>
-      <Social />
+    <main className={styles['post__main']}>
+      <div className={styles['post__post']}></div>
+      <div className={styles['post__comments']}></div>
     </main>
   );
 }
+
+export default Post;
